c1bc3f684837d0b595e65fc33e355eb5aaa72f2e
Larisa <l90vegeta@gmail.com>
2017-03-21 21:04:32 +0100

    Added basic styling to layout

--- app/src/main/java/com/example/android/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizzapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.quizzapp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView titleText = (TextView)findViewById(R.id.quiz_title);
+        Typeface title_font = Typeface.createFromAsset(getAssets(),  "fonts/Oregon-LDO-Bold.ttf");
+        titleText.setTypeface(title_font);
     }
 }
