46d16fbfdad8779ef173fd76f12daf0736e078d6
Larisa <l90vegeta@gmail.com>
2017-03-22 21:54:07 +0100

    Basic functionality for the app. Almost done

--- app/src/main/java/com/example/android/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizzapp/MainActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int score = 0;
@@ -66,6 +70,96 @@ public class MainActivity extends AppCompatActivity {
      * Show the score.
      */
     public void getScoreBtn(View v) {
+        calculateScore();
+        Toast.makeText(this, "Your score is " + score + " out of 7", Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * Calculate the score.
+     */
+    private void calculateScore() {
+        score = 0;
+        checkAnswer1();
+        checkAnswer2();
+        checkAnswer3();
+        checkAnswer4();
+        checkAnswer5();
+        checkAnswer6();
+        checkAnswer7();
+    }
+
+    /**
+     * Check answer for question 1
+     */
+    private void checkAnswer1() {
+        EditText a1EditText = (EditText) findViewById(R.id.a1);
+        if (a1EditText.getText().toString().equals(getString(R.string.a1_editText))) {
+            score += 1;
+        }
+    }
+    /**
+     * Check answer for question 2
+     */
+    private void checkAnswer2() {
+        CheckBox a2CheckBoxA = (CheckBox) findViewById(R.id.a2_a_checkbox);
+        CheckBox a2CheckBoxC = (CheckBox) findViewById(R.id.a2_c_checkbox);
+        CheckBox a2CheckBoxB = (CheckBox) findViewById(R.id.a2_b_checkbox);
+        CheckBox a2CheckBoxD = (CheckBox) findViewById(R.id.a2_d_checkbox);
+        if(a2CheckBoxA.isChecked() && a2CheckBoxC.isChecked() && !a2CheckBoxB.isChecked() && !a2CheckBoxD.isChecked()){
+            score += 1;
+        }
+    }
+
+    /**
+     * Check answer for question 3
+     */
+    private void checkAnswer3() {
+        CheckBox a3CheckBoxA = (CheckBox) findViewById(R.id.a3_a_checkbox);
+        CheckBox a3CheckBoxC = (CheckBox) findViewById(R.id.a3_c_checkbox);
+        CheckBox a3CheckBoxB = (CheckBox) findViewById(R.id.a3_b_checkbox);
+        CheckBox a3CheckBoxD = (CheckBox) findViewById(R.id.a3_d_checkbox);
+        if(!a3CheckBoxA.isChecked() && a3CheckBoxC.isChecked() && a3CheckBoxB.isChecked() && !a3CheckBoxD.isChecked()){
+            score += 1;
+        }
+    }
+
+    /**
+     * Check answer for question 4
+     */
+    private void checkAnswer4() {
+        RadioButton a4RadioButtonD = (RadioButton) findViewById(R.id.a4_d_radio_button);
+        if(a4RadioButtonD.isChecked()){
+            score += 1;
+        }
+    }
+
+    /**
+     * Check answer for question 5
+     */
+    private void checkAnswer5() {
+        RadioButton a5RadioButtonA = (RadioButton) findViewById(R.id.a5_a_radio_button);
+        if(a5RadioButtonA.isChecked()){
+            score += 1;
+        }
+    }
+
+    /**
+     * Check answer for question 6
+     */
+    private void checkAnswer6() {
+        RadioButton a6RadioButtonB = (RadioButton) findViewById(R.id.a6_b_radio_button);
+        if(a6RadioButtonB.isChecked()){
+            score += 1;
+        }
+    }
+
+    /**
+     * Check answer for question 7
+     */
+    private void checkAnswer7() {
+        EditText a7EditText = (EditText) findViewById(R.id.a7);
+        if (a7EditText.getText().toString().equals(getString(R.string.a7_editText))) {
+            score += 1;
+        }
+    }
 }
